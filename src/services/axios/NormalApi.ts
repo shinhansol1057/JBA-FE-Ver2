c176161d50636e9c348a6af229e03e801b01451c
shinhansol <wkdqhrh010@naver.com>
2024-08-16 17:03:29 +0900

    #2 fix: Api.ts 수정

--- src/services/axios/NormalApi.ts
+++ src/services/axios/NormalApi.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from "axios";
 export const NormalApi: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_KEY, // 배포된 서버 사용시
   // baseURL: "http://localhost:8080", // 로컬 서버 사용시
-  header: {
+  headers: {
     "Content-Type": "application/json",
   },
   withCredentials: true,
