530bf9e2001cac7bc9ba188f505093393562bb42
shinhansol <wkdqhrh010@naver.com>
2024-08-27 08:18:51 +0900

    #23 fix: galleryModal imgae size fixed

--- src/containers/gallery/GalleryDetailModal.tsx
+++ src/containers/gallery/GalleryDetailModal.tsx
@@ -10,11 +10,11 @@ type Props = {
 };
 const GalleryDetailModal = ({ setModalOpen, data }: Props) => {
   return (
-    <div className={"flex flex-col relative"}>
+    <div className={"flex flex-col items-center relative max-h-[95vh]"}>
       <div
         className={
           "text-[#EEEEEE] bg-[rgba(0,0,0,0.8)] flex justify-between items-center px-[20px] " +
-          "h-[30px] sm:h-[40px] md:h-[50px]"
+          "h-[30px] sm:h-[40px] md:h-[50px] w-full"
         }
       >
         <AiOutlineDownload
@@ -31,7 +31,7 @@ const GalleryDetailModal = ({ setModalOpen, data }: Props) => {
         alt={data.fileName}
         width={1000}
         height={1000}
-        className={" w-full object-cover"}
+        className={"max-h-[95vh] object-cover"}
       />
       <div
         className={"h-[100vh] w-full absolute top-0 bottom-0 -z-10 "}
