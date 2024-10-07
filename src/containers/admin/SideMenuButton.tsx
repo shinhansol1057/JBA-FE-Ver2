3ed38a8d6df9e284f160137f05465e82a2fc5d9a
hong <daggax8235@gmail.com>
2024-10-07 19:46:37 +0900

    style: 사이드바 닫혔을때랑 열였을때 아이콘 다르게 스탈수정.

--- src/containers/admin/SideMenuButton.tsx
+++ src/containers/admin/SideMenuButton.tsx
@@ -1,13 +1,32 @@
 "use client"
 import { useSidebarStore } from "@/states/SidebarStore"
-import { IoMdMenu } from "react-icons/io"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
+import { cn } from "@/libs/utils/cn"
 
 export const SideMenuButton = () => {
+  const isOpen = useSidebarStore((state) => state.isOpen)
   const toggle = useSidebarStore((state) => state.toggle)
 
   return (
-    <button onClick={toggle} className='lg:hidden'>
-      <IoMdMenu className='h-8 w-8' />
+    <button
+      onClick={toggle}
+      className={cn(
+        "lg:hidden relative w-8 h-8",
+        "transition-colors duration-300",
+      )}
+    >
+      <IoMdMenu
+        className={cn(
+          "absolute inset-0 w-full h-full transition-opacity duration-300",
+          isOpen ? "opacity-0" : "opacity-100",
+        )}
+      />
+      <IoMdClose
+        className={cn(
+          "absolute inset-0 w-full h-full transition-opacity duration-300",
+          isOpen ? "opacity-100" : "opacity-0",
+        )}
+      />
     </button>
   )
 }
