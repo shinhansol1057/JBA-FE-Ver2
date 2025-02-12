23b34fbe1347cfa7349469fb2cacce46b3d9a383
shinhansol <wkdqhrh010@naver.com>
2025-02-12 10:40:12 +0900

    fix: OpenGraph 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,21 @@ export const metadata: Metadata = {
   icons: {
     icon: "/image/logo.jpeg",
   },
+  openGraph: {
+    title: {
+      template: "제주특별자치도농구협회 | %s",
+      default: "제주특별자치도농구협회",
+    },
+    description: "제주특별자치도 농구협회 홈페이지입니다.",
+    images: [
+      {
+        url: "/image/logo.jpeg",
+        width: 1000,
+        height: 1000,
+        alt: "logo",
+      },
+    ],
+  },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -40,6 +55,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           name="naver-site-verification"
           content="9f574bbc7e2631f63d9c1c587bc0353578a32637"
         />
+        <title>제주특별자치도농구협회</title>
       </head>
       <body className={inter.className}>
         <div className={"min-h-[100vh]"}>
